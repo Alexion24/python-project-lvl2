0d670bbd10bf1bfea3c4a5178e3b8e59e778a022
Alexion <alex24bryant@mail.ru>
2022-04-18 20:13:19 +0500

    add files from step 2

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,10 +1,18 @@
 import argparse
 
-parser = argparse.ArgumentParser(
-    usage='gendiff [-h] first_file second_file',
-    description='Generate diff',
-)
-parser.add_argument('first_file')
-parser.add_argument('second_file')
-args = parser.parse_args()
-print(args)
+
+def main():
+    parser = argparse.ArgumentParser(
+        usage='gendiff [-h] [-f FORMAT] first_file second_file',
+        description='Generate diff',
+    )
+    parser.add_argument('first_file')
+    parser.add_argument('second_file')
+    parser.add_argument('-f FORMAT', '--format FORMAT', action='store_true',
+                        help='set format of output')
+    args = parser.parse_args()
+    print(args)
+
+
+if __name__ == '__main__':
+    main()
